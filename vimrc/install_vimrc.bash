85975f7838548a6b7f3286aac836e4b3aa609c22
cen2spub04 <cen2spub04@libpub.multcolib.com>
2024-10-14 12:17:57 -0700

    Doing path stuff, looking at array expansion in a string literal, using for arguments and reference. May go through all the install files to fill file names, would still need to check all the required files are there.

--- vimrc/install_vimrc.bash
+++ vimrc/install_vimrc.bash
@@ -1,16 +1,52 @@
 #!/usr/bin/env bash
 
-# Use PWD to start with and that will likely be default unless path is provided
+# /* 
+#  * Use PWD to start with and that will likely be default unless path is provided
+#  *
+#  * maybe have arguments to alter where the .vimrc goes but have the rest of the
+#  * files in .vim. Look up the locations for global/local+root vimrc and just take into
+#  * acount handling them.
+#  */
 
-# maybe have arguments to alter where the .vimrc goes but have the rest of the
-# files in .vim. Look up the locations for global/local+root vimrc and just take into
-# acount handling them.
+#  /* if .vimrc exists, store it somewhere in case they want to revert
+#   *    - have a functionality to revert?? If reasonable.
+#   */
 
-# if .vimrc exists, store it somewhere in case they want to revert
-#	- have a functionality to revert?? If reasonable.
+FILES_DIR_NAME="sedih_vrc"
+INST_DIR_NAME="vim_files"
 
-# NOTE: This came up using git for windows, .vim is typically there in linux
-if [[ -e !"$HOME/.vim" ]]; then
+
+# /* Probably a better way, load in from a file or something. */
+INSTALL_FILE_NAMES=( 
+	".makefile_vimrc.vim"
+	".dflt_set_cmds_vimrc.vim"
+	".syn_hi_vimrc.vim"
+        ".txt_vimrc.vim"
+        ".c_vimrc.vim"
+        ".py_vimrc.vim"
+	"func_inc.vim"
+	"packadd_includes.vim" 
+)
+
+for $file in 
+
+VIM_FILE_PATHS=(
+	"$HOME/.vim/$INST_DIR_NAME/$FILES_DIR_NAME/${INSTALL_FILE_NAMES[@]}/"
+)
+
+echo -e "${VIM_FILE_PATHS[@]}"
+exit
+
+
+had_vim_folder=1  #/* They prolly do */
+
+
+
+# NOTE: This came up        using git for windows, .vim is typically there in linux
+if [[ -e !"$HOME/.vim/" ]]; then
+	echo "There is no ~/.vim folder, creating it for vimrc installation."
+	mkdir -v "$HOME/.vim/"
+	had_vim_folder=0
 fi
 
 # Check if home dir .vimrc exists
@@ -23,18 +59,20 @@ if [[ -e "$HOME/.vimrc" ]]; then
 	# Perhaps have a choice to add specific functions from func_inc using
 	# flags or some shit
 	# till then:
-	rm -f ~/.vimrc \
+	rm  -rv --one-file-system ~/.vimrc 
 
 
+fi
 
-	      ~/.vim/vimrc_files/.makefile_vimrc.vim \
-	      ~/.vim/vimrc_files/.dflt_set_cmds_vimrc.vim \
-	      ~/.vim/vimrc_files/.syn_hi_vimrc.vim \
-	      ~/.vim/vimrc_files/.txt_vimrc.vim \
-	      ~/.vim/vimrc_files/.c_vimrc.vim \
-	      ~/.vim/vimrc_files/.py_vimrc.vim \
-		  ~/.vim/vimrc_files/func_inc.vim \
-		  ~/.vim/vimrc_files/packadd_includes.vim
+# /* 
+#  * NOTE: We could keep the vimrc_files folder instead of destroying anyone also using
+#  * 	   the folder
+#  * 		We could also just rm -rv the folder.
+#  *
+#  * Keep it simple i suppose? 
+#  */
+if [[ -e "$HOME/.vim/$INST_DIR_NAME/" ]]; then
+	rm -rv --one-file-system "$VIM_FILE_PATHS" 
 fi
 
 
